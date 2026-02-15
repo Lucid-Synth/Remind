a633bd245649a70dfa613328a6cbbcb93bf6679e
Lucid-Synth <uacode75@gmail.com>
2026-02-15 23:44:51 +0530

    Added all content controllers

--- backend/src/controllers/contentController.ts
+++ backend/src/controllers/contentController.ts
@@ -2,6 +2,7 @@ import type { Request, Response } from "express";
 import { notes, yt } from "../drizzle/schema.js";
 import { db } from "../config/db.js";
 import type { AuthRequest } from "../middleware/middleware.js";
+import { eq } from "drizzle-orm";
 
 export const addYoutubeHandler = async (req: AuthRequest, res: Response) => {
   try {
@@ -17,16 +18,18 @@ export const addYoutubeHandler = async (req: AuthRequest, res: Response) => {
       return res.status(401).json({ message: "Unauthorized" });
     }
 
-    const ytVideo = await db.insert(yt).values({
-      title,
-      url,
-      createdBy: req.user.id,
-    })
-    .returning();
+    const ytVideo = await db
+      .insert(yt)
+      .values({
+        title,
+        url,
+        createdBy: req.user.id,
+      })
+      .returning();
 
     res.status(200).json({
       message: "content added successfully",
-      data: ytVideo[0]
+      data: ytVideo[0],
     });
   } catch (error) {
     console.error(error);
@@ -37,40 +40,91 @@ export const addYoutubeHandler = async (req: AuthRequest, res: Response) => {
 };
 
 export const addNotesHandler = async (req: AuthRequest, res: Response) => {
-  try{
-    const { title,userNotes } = req.body;
+  try {
+    const { title, userNotes } = req.body;
 
-    if(!title || !userNotes){
+    if (!title || !userNotes) {
       return res.status(400).json({
-        message: "Required field missing"
-      })
+        message: "Required field missing",
+      });
     }
 
-    if(!req.user?.id){
+    if (!req.user?.id) {
       return res.status(401).json({
-        message: "Unauthorized"
-      })
+        message: "Unauthorized",
+      });
     }
 
-    const addNotes = await db.insert(notes).values({
-      title,
-      userNotes,
-      createdBy: req.user.id
-    }).returning()
+    const addNotes = await db
+      .insert(notes)
+      .values({
+        title,
+        userNotes,
+        createdBy: req.user.id,
+      })
+      .returning();
 
     res.status(200).json({
       message: "content added successfully",
-      data: addNotes[0]
-    })
-  }
-  catch(error){
+      data: addNotes[0],
+    });
+  } catch (error) {
     console.error(error);
     return res.status(500).json({
-      message: "Internal server error"
-    })
+      message: "Internal server error",
+    });
   }
-}
+};
 
-export const getYoutubeHandler = async (req: AuthRequest, res: Response) => {}
+export const getYoutubeHandler = async (req: AuthRequest, res: Response) => {
+  try {
+    const userId = req.user?.id;
 
-export const getNotesHandler = async (req: AuthRequest, res: Response) => {}
+    if (!userId) {
+      return res.status(401).json({
+        message: "Unauthorized",
+      });
+    }
+
+    const data = await db.select().from(yt).where(eq(yt.createdBy, userId));
+
+    return res.status(200).json({
+      success: true,
+      count: data.length,
+      data,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
+
+export const getNotesHandler = async (req: AuthRequest, res: Response) => {
+  try {
+    const userId = req.user?.id;
+
+    if (!userId) {
+      return res.status(401).json({
+        message: "Unauthorized",
+      });
+    }
+
+    const data = await db
+      .select()
+      .from(notes)
+      .where(eq(notes.createdBy, userId));
+
+    return res.status(200).json({
+      success: true,
+      count: data.length,
+      data,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
