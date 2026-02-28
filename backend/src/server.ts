95f8f11436af96dd73f82ed51048d8928de9a9a5
Lucid-Synth <uacode75@gmail.com>
2026-02-28 20:49:58 +0530

    fixes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: "https://remind-rouge.vercel.app/"
+    origin: "https://remind-rouge.vercel.app"
 }))
 
 app.use('/',authRoutes);
