4409a5d94b04381858ad9e11c3228f32cad84dd9
Lucid-Synth <uacode75@gmail.com>
2026-06-03 16:30:20 +0530

    add warmup Request in react

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,8 +3,19 @@ import Hero from "../components/Hero";
 import Features from "../components/Features";
 import Action from "../components/Action";
 import Footer from "../components/Footer";
+import { useEffect } from "react";
+import { Base_Url } from "../config/config";
 
 const Home = () => {
+
+  useEffect(() => {
+    fetch(Base_Url, {
+      method: "GET",
+    }).catch((err) => {
+      console.error("Warm-up request failed:", err);
+    });
+  }, []);
+
   return (
     <div className="min-h-screen bg-[#fafafa] text-slate-900 selection:bg-orange-100">
       <Navbar />
